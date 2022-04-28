64083b6a6414e2b78515eb9e2be8e7175848328f
Lovro Markovic <lovro.markovic@fer.hr>
2022-04-28 11:36:59 +0200

    Add land service for uav_manager

--- nodes/util/uav_manager.cpp
+++ nodes/util/uav_manager.cpp
@@ -6,8 +6,11 @@
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/CommandBool.h>
 #include <uav_ros_msgs/TakeOff.h>
+#include <std_srvs/SetBool.h>
+#include <std_srvs/Empty.h>
 #include <sensor_msgs/Joy.h>
 #include <memory>
+
 
 void uav_ros_control::UAVManager::onInit()
 {
@@ -21,15 +24,20 @@ void uav_ros_control::UAVManager::onInit()
     ros_util::CreateTopicHandlerMutexed<nav_msgs::Odometry>(nh, "odometry", ODOM_TIMEOUT);
   m_mavros_state_handler =
     ros_util::CreateTopicHandlerMutexed<mavros_msgs::State>(nh, "mavros/state");
+  m_tracker_state_handler =
+    ros_util::CreateTopicHandlerMutexed<std_msgs::String>(nh, "tracker/status");
 
   // Initialize services
   m_mode_client    = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
   m_arming_client  = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
   m_takeoff_client = nh.serviceClient<uav_ros_msgs::TakeOff>("takeoff");
+  m_land_client    = nh.serviceClient<std_srvs::SetBool>("land");
+  m_tracker_reset_client = nh.serviceClient<std_srvs::Empty>("tracker/reset");
 
   // Initialize servers
   m_armtakeoff_server =
-    nh.advertiseService("arm_and_takeoff", &UAVManager::arm_and_takeoff_cb, this);
+    nh.advertiseService("uav_manager/takeoff", &UAVManager::arm_and_takeoff_cb, this);
+  m_land_server = nh.advertiseService("uav_manager/land", &UAVManager::land_cb, this);
 
   // Initialize publishers
   m_joy_publisher = nh.advertise<sensor_msgs::Joy>("joy", 1);
@@ -183,6 +191,100 @@ bool uav_ros_control::UAVManager::arm_and_takeoff_cb(
   return true;
 }
 
+bool uav_ros_control::UAVManager::land_cb(uav_ros_msgs::Land::Request&  req,
+                                          uav_ros_msgs::Land::Response& resp)
+{
+  const auto set_response = [&](bool status, std::string message) {
+    resp.message = message;
+    resp.success = status;
+  };
+
+  // Check if odometry is alive
+  if (!m_odom_handler->isResponsive()) {
+    set_response(false, m_odom_handler->getName() + " unresponsive!");
+    return true;
+  }
+
+  // Check if mavros state
+  if (!m_mavros_state_handler->isResponsive()) {
+    set_response(false, m_mavros_state_handler->getName() + " unresponsive!");
+    return true;
+  }
+
+  // Check the carrot state
+  if (!m_carrot_state_handler->isResponsive()) {
+    set_response(false, m_carrot_state_handler->getName() + " unresponsive!");
+    return true;
+  }
+
+  // Check if land client exists
+  if (!m_land_client.exists()) {
+    set_response(false, m_land_client.getService() + " does not exist!");
+    return true;
+  }
+
+  // Check if we are in OFFBOARD_MODE
+  if (m_mavros_state_handler->getData().mode != OFFBOARD_MODE) {
+    set_response(false, "UAV not in " + std::string(OFFBOARD_MODE));
+    return true;
+  }
+
+  // Check if we are in position hold
+  if (m_carrot_state_handler->getData().data != POSITION_HOLD) {
+    set_response(false, "Carrot not in " + std::string(POSITION_HOLD));
+    return true;
+  }
+
+  // TODO(lmark): Stop the mission before stopping the trajectory
+
+  // Check if trajectory is being executed
+  const auto tracker_status = m_tracker_state_handler->getData().data;
+  if (tracker_status == TRACKER_ACTIVE) {
+    if (req.force_land) {
+      // Force disable the trajectory
+      if (!m_tracker_reset_client.exists()) {
+        set_response(false, "trying to force the landing, but unable to stop tracker.");
+        return true;
+      }
+
+      std_srvs::Empty reset_request;
+      auto            reset_success = m_tracker_reset_client.call(reset_request);
+      if (!reset_success) {
+        set_response(false, "trying to force the landing, but unable to stop tracker.");
+        return true;
+      }
+
+      // Trajectory should be stopped at this point :)
+      // TODO(lmark): Should probably add a different service type for stopping the
+      // trajectory
+
+    } else {
+      // We do not want to force landing if a trajectory is executed
+      set_response(false, "Unable to land, tracker is active");
+      return false;
+    }
+  }
+
+  // From this point everything is ready for landing
+  // ----------------------------------------------
+
+  std_srvs::SetBool land_request;
+  land_request.request.data = true;
+  auto land_success         = m_land_client.call(land_request);
+  if (!land_success) {
+    set_response(false, "Unable to request land.");
+    return true;
+  }
+
+  if (!land_request.response.success)
+  {
+    set_response(false, "Land failed with message " + land_request.response.message);
+    return true;
+  }
+  
+  set_response(true, "Land successful");
+  return true;
+}
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(uav_ros_control::UAVManager, nodelet::Nodelet)
--- nodes/util/uav_manager.hpp
+++ nodes/util/uav_manager.hpp
@@ -1,5 +1,6 @@
 #include "ros/forwards.h"
 #include "ros/publisher.h"
+#include "ros/service_callback_helper.h"
 #include "ros/service_client.h"
 #include "ros/service_server.h"
 #include <nodelet/nodelet.h>
@@ -8,6 +9,7 @@
 #include <std_msgs/String.h>
 #include <mavros_msgs/State.h>
 #include <uav_ros_msgs/ArmAndTakeoff.h>
+#include <uav_ros_msgs/Land.h>
 #include <uav_ros_lib/topic_handler.hpp>
 #include <mutex>
 
@@ -22,24 +24,34 @@ private:
   static constexpr auto  PRE_TAKEOFF_STATUS       = "CARROT_ON_LAND";
   const std::vector<int> CARROT_ACTIVATION_VECTOR = { 0, 0, 0, 0, 0, 1 };
   static constexpr auto  OFFBOARD_MODE            = "GUIDED_NOGPS";
+  static constexpr auto  TRACKER_ACTIVE           = "ACTIVE";
+  static constexpr auto  POSITION_HOLD            = "HOLD";
   static constexpr auto  ODOM_TIMEOUT             = 0.1;
   static constexpr auto  WAIT_DURATION            = 0.1;
 
-  // Service servers
+  // Takeoff server
   ros::ServiceServer m_armtakeoff_server;
   std::mutex         m_armtakeoff_mutex;
   bool               arm_and_takeoff_cb(uav_ros_msgs::ArmAndTakeoff::Request&  req,
                                         uav_ros_msgs::ArmAndTakeoff::Response& resp);
-  
+
+  // Land server
+  ros::ServiceServer m_land_server;
+  std::mutex         m_land_mutex;
+  bool land_cb(uav_ros_msgs::Land::Request& req, uav_ros_msgs::Land::Response& resp);
+
   // Subscriber handlers
   ros_util::TopicHandlerMutexed<nav_msgs::Odometry>::Ptr m_odom_handler;
   ros_util::TopicHandlerMutexed<std_msgs::String>::Ptr   m_carrot_state_handler;
   ros_util::TopicHandlerMutexed<mavros_msgs::State>::Ptr m_mavros_state_handler;
+  ros_util::TopicHandlerMutexed<std_msgs::String>::Ptr   m_tracker_state_handler;
 
   // Service clients
   ros::ServiceClient m_takeoff_client;
+  ros::ServiceClient m_land_client;
   ros::ServiceClient m_arming_client;
   ros::ServiceClient m_mode_client;
+  ros::ServiceClient m_tracker_reset_client;
 
   // Publishers
   ros::Publisher m_joy_publisher;
